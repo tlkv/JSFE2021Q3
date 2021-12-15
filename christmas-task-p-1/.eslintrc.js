c3afce2db5d75e6f6c0d008b42922193ce82feb7
A0009 <aaa144855@gmail.com>
2021-12-16 02:34:31 +0300

    init: start christmas-task-part1

---
+++ christmas-task-p-1/.eslintrc.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "ignorePatterns": ["*.config.js"],
+    "rules": {"@typescript-eslint/no-explicit-any": "error"}
+};
