eab60b2e32a52bab1f40c2215bf72f72e043fd8d
A0009 <aaa144855@gmail.com>
2022-01-19 20:44:57 +0300

    init: start async race task

---
+++ async-race-st2/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['*.config.js'],
+  rules: { '@typescript-eslint/no-explicit-any': 'error' },
+};
